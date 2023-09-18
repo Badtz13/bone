e3e0245840ea466bfbf60087e8cd3be11f3f3cc3
Badtz <badtzmaru139@gmail.com>
2023-09-17 17:54:53 -0700

    boneforge texture

--- src/main/java/me/woach/bone/blocks/BoneForge.java
+++ src/main/java/me/woach/bone/blocks/BoneForge.java
@@ -1,9 +1,14 @@
 package me.woach.bone.blocks;
 
+import me.woach.bone.screens.BoneForgeScreenHandler;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.screen.NamedScreenHandlerFactory;
+import net.minecraft.screen.ScreenHandlerContext;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
@@ -13,14 +18,24 @@ import net.minecraft.world.World;
 
 public class BoneForge extends Block {
     public BoneForge() {
-        super(FabricBlockSettings.create().strength(4.0f).requiresTool());
+        super(FabricBlockSettings.create().strength(4.0f).requiresTool().luminance(8).sounds(BlockSoundGroup.METAL));
     }
 
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand,
             BlockHitResult hit) {
         if (!world.isClient) {
-            player.sendMessage(Text.of("Hello, world!"), false);
+            player.openHandledScreen(new NamedScreenHandlerFactory() {
+                @Override
+                public Text getDisplayName() {
+                    return Text.of("Bone Forge");
+                }
+
+                @Override
+                public BoneForgeScreenHandler createMenu(int syncId, PlayerInventory inv, PlayerEntity player) {
+                    return new BoneForgeScreenHandler(syncId, inv, ScreenHandlerContext.create(world, pos));
+                }
+            });
         }
 
         return ActionResult.SUCCESS;
