dc6d53580987387bef7ac3354db60dc2c1449de4
langedev <public@daltonlange.com>
2023-09-18 15:56:13 -0700

    Got pool modification working through mixins. Still need to remove minecraft:bones, and datapack bones

--- src/main/java/me/woach/bone/registries/BoneRegistry.java
+++ src/main/java/me/woach/bone/registries/BoneRegistry.java
@@ -3,22 +3,33 @@ package me.woach.bone.registries;
 import me.woach.bone.datapack.AbstractBone;
 import net.minecraft.util.Identifier;
 
+import java.util.Collection;
 import java.util.HashMap;
 
 public class BoneRegistry {
     private static final HashMap<Identifier, AbstractBone> idToBone = new HashMap<>();
+
+    private static final BoneLootTableManager lootTable = new BoneLootTableManager();
 
     public static AbstractBone register(Identifier id, AbstractBone bone) {
         if(idToBone.containsKey(id))
             throw new IllegalArgumentException("Duplicate bone id. Tried to register: " + id.toString());
 
         idToBone.put(id, bone);
+        lootTable.addBoneLoot(bone);
         return bone;
+    }
+
+    public static Collection<AbstractBone> getAll() {
+        return idToBone.values();
     }
 
     public static AbstractBone get(Identifier id) { return idToBone.get(id); }
 
-    public static void empty() { idToBone.clear(); }
+    public static void empty() {
+        lootTable.removeBoneLoot();
+        idToBone.clear();
+    }
 
     public static int size() { return idToBone.size(); }
 }
