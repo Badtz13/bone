0bd9e79b1efd6710af5c356190b393463bf3b52c
Badtz <badtzmaru139@gmail.com>
2023-10-13 19:55:44 -0700

    add blockstates and textures

--- src/main/java/me/woach/bone/blocks/BoneFireBlock.java
+++ src/main/java/me/woach/bone/blocks/BoneFireBlock.java
@@ -1,7 +1,12 @@
 package me.woach.bone.blocks;
 
 import me.woach.bone.Bone;
-import net.minecraft.block.*;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.MapColor;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.piston.PistonBehavior;
 import net.minecraft.entity.Entity;
@@ -10,6 +15,9 @@ import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.EnumProperty;
+import net.minecraft.util.StringIdentifiable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
@@ -17,6 +25,26 @@ import net.minecraft.world.World;
 import net.minecraft.world.WorldEvents;
 
 public class BoneFireBlock extends Block {
+
+    public enum FireType implements StringIdentifiable {
+        JORD("jord"),
+        AEGIR("aegir"),
+        STJARNA("stjarna");
+
+        private final String name;
+
+        FireType(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String asString() {
+            return this.name;
+        }
+    }
+
+    public static final EnumProperty<FireType> TYPE = EnumProperty.of("type", FireType.class);
+
     protected static final VoxelShape BASE_SHAPE = Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 1.0, 16.0);
     private final float damage;
 
@@ -24,6 +52,12 @@ public class BoneFireBlock extends Block {
         super(AbstractBlock.Settings.create().mapColor(MapColor.YELLOW).replaceable().noCollision().breakInstantly()
                 .luminance(state -> 15).sounds(BlockSoundGroup.WOOL).pistonBehavior(PistonBehavior.DESTROY));
         this.damage = 2;
+        setDefaultState(getStateManager().getDefaultState().with(TYPE, FireType.JORD));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(TYPE);
     }
 
     @Override
@@ -50,7 +84,7 @@ public class BoneFireBlock extends Block {
 
     @Override
     public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
-        if(checkConsumptionParameters(world, entity, pos)) {
+        if (checkConsumptionParameters(world, entity, pos)) {
             BoneForgeBlockEntity forge = (BoneForgeBlockEntity) world.getBlockEntity(pos.up());
             ItemStack essence = ((ItemEntity) entity).getStack();
             forge.setEssenceLevel(essence);
